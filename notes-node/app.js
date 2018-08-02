9839539ff10c98e56649c740d3616405e2d9a427
Manuel Garcia <magarcia@eink.com>
2018-08-01 18:36:02 -0700

    Third party modules.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,14 +1,21 @@
 console.log("Start");
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-console.log(os.userInfo());
-var user = os.userInfo();
-fs.appendFile('greetings.txt',  `Hello  ${user.username} !` , function (err) {
-        if(err) {
-            console.log("Unable to write file");
-        }
-} );
+// console.log( _.isString(true) );
+// console.log( _.isString("Hello"));
 
-console.log(notes.add(24,7));
+console.log(_.uniq([1,1,2,2,3,3,4,4,5,5]));
+
+// console.log(os.userInfo());
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt',  `Hello  ${user.username} !` , function (err) {
+//         if(err) {
+//             console.log("Unable to write file");
+//         }
+// } );
+//
+// console.log(notes.add(24,7));
