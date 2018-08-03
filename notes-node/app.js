8dbc3c52e1a8594f160c107a2436b90716519f5b
Manuel Garcia <magarcia@eink.com>
2018-08-02 18:41:42 -0700

    Parsing arguments.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,21 +1,27 @@
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
+const yargs = require('yargs');
+
+const argv = yargs.argv;
 
 var command = process.argv[2];
 
 console.log(command);
-console.log(process.argv);
+console.log('Yargs',argv);
 
 if (command === 'add') {
-  console.log("adding new node")
-
+  console.log("adding new note")
+  notes.addNote(argv.title, argv.body);
 } else if(command === 'list') {
   console.log("listing nodes");
+  notes.getAll();
 } else if(command === 'read') {
-  console.log("read node")
+  console.log("read note")
+  notes.readNote(argv.title);
 } else if(command === 'remove') {
   console.log("remove node")
+  notes.removeNote(argv.title);
 }
 else {
   console.log("command not recognized");
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,1 +1,22 @@
 console.log("Starting nodes.js");
+
+var addNote = (title,body) => {
+    console.log("Adding note ", title, body);
+};
+
+var getAll = () => {
+  console.log("getting all");
+}
+
+var readNote = (title) => {
+  console.log("Reading note : ", title);
+}
+var removeNote = (title) => {
+  console.log("Removing note : ", title)
+}
+module.exports = {
+  addNote,
+  getAll,
+  readNote,
+  removeNote
+};
