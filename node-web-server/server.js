a1713a89f5ba051f29e76a3878c0f02f59e11ec3
Manuel Garcia <alexgarcia2k@gmail.com>
2018-08-16 18:27:21 -0700

    first commit on node webserver.

---
+++ node-web-server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/' , (req,res)=> {
+  //res.send("<h1>hello express</h1>");
+  res.send({
+    name: "Alex",
+    likes: [
+      'bowling',
+      'coding'
+    ]
+  });
+});
+
+app.get('/about',(req,res)=> {
+  res.send("About page");
+});
+
+app.get('/bad',(req,res)=> {
+  res.send({
+    errorMessage : "Error handling request"
+  });
+});
+app.listen(3000);
